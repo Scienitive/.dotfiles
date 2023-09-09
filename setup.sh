a37e2b1c55662ad40242d5dbd7300a27338be0cc
Scienitive <scienitive77@gmail.com>
2023-09-09 14:09:02 +0300

    oh my zsh removed

--- setup.sh
+++ setup.sh
@@ -3,7 +3,5 @@ ln -svf ~/.dotfiles/.zshrc ~/.zshrc
 mkdir ~/.config
 rm -rf ~/.config/nvim
 ln -svf ~/.dotfiles/.config/nvim ~/.config
-rm -rf ~/.oh-my-zsh
-ln -svf ~/.dotfiles/.oh-my-zsh ~/.
 rm -rf ~/.tmux
 ln -svf ~/.dotfiles/.tmux ~/.
