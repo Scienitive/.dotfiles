9d332589a8c80b8e65434bfeceec75ba73955a6d
Scienitive <scienitive77@gmail.com>
2023-09-09 14:05:51 +0300

    added oh-my-zsh and tmux

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 ln -svf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -svf ~/.dotfiles/.zshrc ~/.zshrc
+mkdir ~/.config
+rm -rf ~/.config/nvim
 ln -svf ~/.dotfiles/.config/nvim ~/.config
+rm -rf ~/.oh-my-zsh
+ln -svf ~/.dotfiles/.oh-my-zsh ~/.
+rm -rf ~/.tmux
+ln -svf ~/.dotfiles/.tmux ~/.
