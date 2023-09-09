d1fa0b5b11ee97c614a502c5885f74bee0b16a8f
Scienitive <scienitive77@gmail.com>
2023-09-09 14:12:59 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -3,5 +3,4 @@ ln -svf ~/.dotfiles/.zshrc ~/.zshrc
 mkdir ~/.config
 rm -rf ~/.config/nvim
 ln -svf ~/.dotfiles/.config/nvim ~/.config
-rm -rf ~/.tmux
-ln -svf ~/.dotfiles/.tmux ~/.
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
